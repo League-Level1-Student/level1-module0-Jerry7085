4f6f040c9fa70a8d0d5a044f1ac1f511f66cdca6
root <root@c0e0499356bc>
2020-07-20 22:00:52 +0000

    .

--- src/_01_houses/Houses.java
+++ src/_01_houses/Houses.java
@@ -1,7 +1,41 @@
 package _01_houses;
 
+import java.awt.Color;
+import java.util.Random;
+
+import org.jointheleague.graphical.robot.Robot;
+
 public class Houses {
 	public void run() {
+	
+	
+		building();
+		building();
+		building();
+		building();
+		building();
 		
 	}
+
+
+static void building() {
+	Robot j = new Robot();
+	Random ran = new Random();
+	int rand = ran.nextInt(120-30+1)+30;
+	
+	
+	j.setSpeed(30);
+	j.penDown();
+	j.setAngle(0);
+	j.setRandomPenColor();
+	j.move(rand);
+	j.turn(90);
+	j.move(30);
+	j.turn(90);
+	j.move(rand);
+	j.turn(-90);
+	j.setPenColor(Color.green);
+	j.move(30);
+	
+}
 }
